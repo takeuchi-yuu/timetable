a60d475e901450178d6230b258ae7bac7049a1be
takeuchi-yuu <zhuneikaifayong@gmail.com>
2025-03-17 17:42:11 +0900

    カスタム要素を登録

--- src/register.mjs
+++ src/register.mjs
@@ -1,3 +1,7 @@
 import { HomePage } from "./pages/home.mjs";
+import { ClassListPage } from "./pages/class-list.mjs";
+import { ClassEditPage } from "./pages/class-edit.mjs";
 
 customElements.define("home-page", HomePage);
+customElements.define("class-list-page", ClassListPage);
+customElements.define("class-edit-page", ClassEditPage);
