fbaea877c5323111f54a368e160d99fa227245cf
takeuchi-yuu <zhuneikaifayong@gmail.com>
2025-03-17 16:57:29 +0900

    存在しないハッシュに対応

---
+++ main.mjs
@@ -0,0 +1,21 @@
+import { routes } from "./src/routes.mjs";
+
+async function onHashChange() {
+  const hash = window.location.hash;
+  console.log(hash);
+  if (hash === "") {
+    window.location.hash = "#home";
+  }
+  const page = routes[hash];
+  if (!page) {
+    console.warn("unknown route");
+    window.location.hash = "#home";
+    return;
+  }
+
+  const appRoot = document.querySelector("app-root");
+  appRoot.innerHTML = page;
+}
+
+window.addEventListener("hashchange", onHashChange);
+onHashChange();
