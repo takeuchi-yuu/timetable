d0fd360afd8e6fee51677a1c8ba5f0525fff2151
takeuchi-yuu <zhuneikaifayong@gmail.com>
2025-03-18 11:41:59 +0900

    時間割画面用のCSSを追加

--- src/pages/home.mjs
+++ src/pages/home.mjs
@@ -1,6 +1,28 @@
+import { basicStyle } from "../shared/style.mjs";
 export class HomePage extends HTMLElement {
   /** @type { ShadowRoot | undefined } */
   shadowRoot = undefined;
+
+  css = () => /* css */ `
+    ${basicStyle}
+
+    :host .home {
+      width: 100%;
+      height: 100%;
+      display: grid;
+      grid-auto-flow: column;
+      grid-template-columns: repeat(5, 1fr);
+      grid-template-rows: 32px repeat(4, 1fr);
+      place-content: center;
+
+      & > .class-item {
+        width: 100%;
+        height: 100%;
+        display: grid;
+        place-content: center;
+      }
+    }
+  `;
 
   html = () => /* html */ `
     <span>home page</span>
