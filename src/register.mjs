f4f5e29ac7148f4263549185448ea2402c5917b0
takeuchi-yuu <zhuneikaifayong@gmail.com>
2025-03-18 10:43:26 +0900

    class-list-item要素をカスタム要素として登録

--- src/register.mjs
+++ src/register.mjs
@@ -1,7 +1,9 @@
 import { HomePage } from "./pages/home.mjs";
 import { ClassListPage } from "./pages/class-list.mjs";
 import { ClassEditPage } from "./pages/class-edit.mjs";
+import { ClassListItem } from "./components/class-list-item.mjs";
 
 customElements.define("home-page", HomePage);
 customElements.define("class-list-page", ClassListPage);
 customElements.define("class-edit-page", ClassEditPage);
+customElements.define("class-list-item", ClassListItem);
